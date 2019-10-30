bdd9bca158ab422c03ebafcfcbfe1fbb20ad17d4
rpg-tips <jmnavarro90@outlook.com>
2019-10-31 00:33:50 +0100

    Looooots of functions from the Solo Adventurer's Toolbox. Only ones left that I'm interested on are the minor terrain ones.

--- R/solo_adventurer_toolbox.R
+++ R/solo_adventurer_toolbox.R
@@ -43,7 +43,7 @@ toolbox_weather <- function(season = "autumn"){
 toolbox_settlement <- function(){
   sample(x = c("Large camp / caravan (25% abandoned), 1-20",
                "Cottage, 1-10",
-               "Large encampent, 50-100",
+               "Large encampment, 50-100",
                "Hamlet, 50-150",
                "Work crew, 50-200",
                "Stationed garrison, 100-300",
@@ -119,140 +119,16 @@ toolbox_rumour <- function(){
                          "In the capital of the realm",
                          "In the next realm"))))
  }
+ if(subject == "Creature"){
+   return("It's about a creature!")
+ }
+ if(subject == 'Person'){
+   return("It's about someone!")
+ }
+ if(subject == 'Thing'){
+   return("It's about something!")
+ }
 }
 
-#' @export
-toolbox_terrain_arctic <- function(){
-  tmp <- data.frame(num = c(60,65,70,75,80,85,90,95,100),
-                   feat = c("Continuing arctic terrain, 5 miles",
-                           "Icy hills (roll hills and interpret... arctically). 1d4 m.",
-                           "Small mountain range / foothills. 1d4 m.",
-                           "Waterway (probably frozen). 1d4 m",
-                           "Small wood. 1d4 m.",
-                           "Rocky outcrop. 1d4 m.",
-                           "Unmarked settlement",
-                           "D6: structure / monument / interesting landscape feature",
-                           "Monster Lair"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_coastal <- function(){
-  tmp <- data.frame(num = c(60,65,70,75,80,85,90,95,100),
-                    feat = c("Continuing coastal terrain, 5 miles",
-                             "Hills, with cliffs perhaps. 1d4 m.",
-                             "Gully. 1d4 m.",
-                             "Waterway. 1d4 m",
-                             "Small wood. 1d4 m.",
-                             "Rocky outcrop. 1d4 m.",
-                             "Unmarked settlement",
-                             "D6: structure / monument / interesting landscape feature",
-                             "Monster Lair"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_desert <- function(){
-  tmp <- data.frame(num = c(70,75,80,85,90,95,100),
-                    feat = c("Continuing desert, 5 miles",
-                             "Oasis (roll Oasis table). 1d4 m.",
-                             "Small mountain range (dry, no snow). 1d4 m.",
-                             "Rocky outcrop. 1d4 m",
-                             "Unmarked settlement",
-                             "D6: structure / monument / interesting landscape feature",
-                             "Monster Lair"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_forest <- function(){
-  tmp <- data.frame(num = c(52,56,60,64,68,72,76,80,84,88,92,96,100),
-                    feat = c("Continuing forest, 5 miles",
-                             "Small clearing",
-                             "Large clearing",
-                             "Small wooded gully, 1d4 m.",
-                             "Large wooded gully, 1d4 m.",
-                             "Waterway",
-                             "Lake, 1d4 m.",
-                             "Clearfelled area, 1d4 m.",
-                             "Rocky outcrop, 1d4 m.",
-                             "Swamp, 1d4 m.",
-                             "Unmarked forest settlement",
-                             "D6: structure / monument / monster lair",
-                             "Interesting Landscape feature"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_grassland <- function(){
-  tmp <- data.frame(num = c(56,60,64,68,72,76,80,84,88,92,96,100),
-                    feat = c("Continuing grassland, 5 miles",
-                             "Hills",
-                             "Swamp",
-                             "Gully, 1d4 m.",
-                             "Waterway, 1d4 m.",
-                             "Lake",
-                             "Small wood, 1d4 m.",
-                             "Rocky outcrop, 1d4 m.",
-                             "Small mountain range / foothills, 1d4 m.",
-                             "Unmarked settlement",
-                             "D6: structure / monument / interesting landscape feature",
-                             "Monster Lair"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_hills <- function(){
-  tmp <- data.frame(num = seq(55, 100, by = 5),
-                    feat = c("Continuing hills, 5 miles",
-                             "Gully, 1d4 m.",
-                             "Waterway, 1d4 m.",
-                             "Lake, 1d4 m.",
-                             "Small wood, 1d4 m.",
-                             "Rocky outcrop, 1d4 m.",
-                             "Small mountain range / Foothills, 1d4 m.",
-                             "Unmarked settlement",
-                             "D6: structure / monument / monster lair",
-                             "Interesting Landscape feature"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_mountains <- function(){
-  tmp <- data.frame(num = seq(55, 100, by = 5),
-                    feat = c("Continuing mountains, 5 miles",
-                             "Gully, 1d4 m.",
-                             "Waterway, 1d4 m.",
-                             "Lake, 1d4 m.",
-                             "Small wood, 1d4 m.",
-                             "Rocky outcrop, 1d4 m.",
-                             "Small mountain range / Foothills, 1d4 m.",
-                             "Unmarked settlement",
-                             "D6: structure / monument / monster lair",
-                             "Interesting Landscape feature"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
-
-#' @export
-toolbox_terrain_swamp <- function(){
-  tmp <- data.frame(num = c(51,58,65,72,79,86,93,100),
-                    feat = c("Continuing swamp, 5 miles",
-                             "Waterway, 1d4 m.",
-                             "Lake, 1d4 m.",
-                             "Small wood, 1d4 m.",
-                             "Rocky outcrop, 1d4 m.",
-                             "Unmarked settlement",
-                             "D6: structure / monument / monster lair",
-                             "Interesting Landscape feature"), stringsAsFactors = F)
-  dice <- sample(1:100, size = 1)
-  tmp$feat[which(tmp$num >= dice)[1]]
-}
 
 
